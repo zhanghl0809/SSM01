1596280507bad8c88004aa76ac4f72b808626bf8
zhanghl <610089335@qq.com>
2019-05-26 18:41:41 +0800

    jixin

--- src/main/java/com/ssm/controller/UserController.java
+++ src/main/java/com/ssm/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.ssm.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.ssm.Entity.User;
 import com.ssm.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("/")
@@ -32,5 +36,14 @@ public class UserController {
     public String welcome1(){
         User u = userService.findUserById(4);
         return "/welcome1";
+    }
+    @ResponseBody
+    @RequestMapping("/wx")
+    public Object wx(){
+        HashMap<String, String> map = new HashMap<String, String>();
+        User u = userService.findUserById(1);
+        map.put("name",u.getUsername());
+        JSONObject json = JSONObject.parseObject(JSON.toJSONString(map));//把map转为json数据
+        return json;
     }
 }
